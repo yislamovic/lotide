d4f7ff3c518eaf7edaa623ab32ec9bc9e63b3d70
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-07-25 19:42:19 -0400

    new file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,18 @@
+const eqArrays = function (array1, array2) {
+  let bool = [];
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        bool[i] = true;
+      }
+      else {
+        bool[i] = false;
+      }
+      i++;
+    }
+  }
+  return !bool.includes(false);
+}
 const eqObjects = function (object1, object2) {
   const keysObj1 = Object.keys(object1);
   const keysObj2 = Object.keys(object1);
@@ -12,10 +27,17 @@ const eqObjects = function (object1, object2) {
         return eqArrays(keyArr1, keyArr2);
       }
       else {
-        if (object1[key] === object2[key]) {
+        let type0 = typeof object1[key];
+        let type1 = typeof object2[key];
+        if (object1[key] !== object2[key]) {
+          return false;
         }
         else {
-          return false;
+          if(type0 && type1 === Function){
+            if(object1[key].toString() !== object2[key].toString()){
+              return false;
+            }
+          }
         }
       }
     }
@@ -23,5 +45,18 @@ const eqObjects = function (object1, object2) {
   }
 };
 const assertObjectsEqual = function(actual, expected) {
-  // Implement me!
+  const inspect = require('util').inspect;
+  let bool = eqObjects(actual, expected);
+  return (bool !== (false)) ? `✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+  : `❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
 };
+//passing assertions
+console.log(assertObjectsEqual({a: 1, b: 2}, {a: 1, b: 2}));
+console.log(assertObjectsEqual({a: 1, b: 2, c: '3'}, {a: 1, b: 2, c: '3'}));
+// work in progress feature //check equality for functions 
+//console.log(assertObjectsEqual({a: 1, b: x => x}, {a: 1, b: x => x}));
+//Unequal Objects
+console.log('\n');
+console.log(assertObjectsEqual({a: 1, b: 2}, {a: 1, b: 3}));
+console.log(assertObjectsEqual({a: 1, b: 2, c: 3}, {a: 1, b: 2}));
+console.log(assertObjectsEqual({a: 1, b: 2}, {a: '1', b: '3'}));
